b172a6dae50d6b315949fe34385446e079846aa2
lukorito <jonesdeelder@gmail.com>
2022-02-17 02:52:34 +0300

    Rotation pint

---
+++ Arrays and Strings/Rotation_point_Linear_Search.py
@@ -0,0 +1,36 @@
+  #* CodeCademy Question
+
+"""Write a function count_rotations() which has one parameter rotated_list.
+
+count_rotations() should return the index where the sorted list would begin."""
+
+#* -------My Solution---------
+# find rotation point 
+# No time/space requirements
+# return index of "rotation point" element
+
+def rotation_point(rotated_list):
+  j = 0
+  while j < len (rotated_list):
+    if rotated_list[j] < rotated_list[j-1]:
+      return j
+    if rotated_list[j] > rotated_list[j-1]:
+      j+=1
+  return j
+
+
+#* -------CodeCademy Solution---------
+
+# def rotation_point(rotated_list):
+#   rotation_idx = 0
+#   for i in range(len(rotated_list)):
+#       if rotated_list[i] < rotated_list[rotation_idx]:
+#         rotation_idx = i
+#   return rotation_idx
+
+#### TESTS SHOULD ALL BE TRUE ####
+print("{0}\n should equal \n{1}\n {2}\n".format(rotation_point(['a', 'b', 'c', 'd', 'e', 'f']), 0, rotation_point(['a', 'b', 'c', 'd', 'e', 'f']) == 0))
+
+print("{0}\n should equal \n{1}\n {2}\n".format(rotation_point(['c', 'd', 'e', 'f', 'a']), 4, rotation_point(['c', 'd', 'e', 'f', 'a']) == 4))
+
+print("{0}\n should equal \n{1}\n {2}\n".format(rotation_point([13, 8, 9, 10, 11]), 1, rotation_point([13, 8, 9, 10, 11]) == 1))
