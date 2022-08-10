c27074f4473f1ee1cba7a9ee01e1be82e413f424
lukorito <jonesdeelder@gmail.com>
2022-08-10 16:45:05 +0300

    Linked lists

--- Linked Lists/Doubly_Linked_lists.py
+++ Linked Lists/Doubly_Linked_lists.py
@@ -1,51 +1,80 @@
+# class Node:
+#     def __init__(self, data):
+#         self.data = data
+#         self.next = None
+#         self.prev = None
 
-class Node:
-    def __init__(self, data):
-        self.data = data
-        self.prev = None
-        self.next = None
-
-
-class LinkedList:
-    def __init__(self):
-        self.head = None
-
-    def createList(self, arr):
-        start = self.head
-        n = len(arr)
-
-        temp = start
-        i = 0
-
-        while (i<n):
-            newNode = Node(arr[i])
-            if (i==0):
-                start = newNode
-                temp = start
-            else:
-                temp.next=newNode
-                newNode.prev = temp
-                temp = temp.next
-            i += 1
-        self.head = start
-        return start
-    
-    def printList(self):
-        temp = self.head
-        linked_list = ""
-        while (temp):
-            linked_list += (str(temp.data) + " <=> ")
-            temp = temp.next
-
-        print(linked_list)
-
-    def insert
+#     def __str__(self):
+#         return str(self.data)
 
 
-arr = [1,2,3,4,5]
+# class DoublyLinkedList:
+#     def __init__(self):
+#         self.head = None
+#         self.tail = None
+#         self.size = 0
 
-llist = LinkedList()
+#     def __str__(self):
+#         if self.head is None:
+#             return "Empty"
+#         else:
+#             return str(self.head.data)
 
-llist.createList(arr)
+#     def add_first(self, data):
+#         new_node = Node(data)
+#         if self.head is None:
+#             self.head = new_node
+#             self.tail = new_node
+#         else:
+#             new_node.next = self.head
+#             self.head.prev = new_node
+#             self.head = new_node
+#         self.size += 1
 
-llist.printList()
+#     def add_last(self, data):
+#         new_node = Node(data)
+#         if self.head is None:
+#             self.head = new_node
+#             self.tail = new_node
+#         else:
+#             new_node.prev = self.tail
+#             self.tail.next = new_node
+#             self.tail = new_node
+#         self.size += 1
+
+#     def add_at(self, data, index):
+#         if index < 0 or index > self.size:
+#             print("Index out of range")
+#             return
+#         new_node = Node(data)
+#         if index == 0:
+#             self.add_first(data)
+#         elif index == self.size:
+#             self.add_last(data)
+#         else:
+#             curr = self.head
+#             for i in range(index):
+#                 curr = curr.next
+#             new_node.next = curr
+#             new_node.prev = curr.prev
+#             curr.prev.next = new_node
+#             curr.prev = new_node
+#             self.size += 1
+
+#     def remove_first(self):
+#         if self.head is None:
+#             print("Empty")
+#             return
+#         else:
+#             self.head = self.head.next
+#             self.head.prev = None
+#             self.size -= 1
+
+#     def remove_last(self):
+#         if self.head is None:
+#             print("Empty")
+#             return
+#         else:
+#             self.tail = self.tail.prev
+#             self.tail.next = None
+#             self.size -= 1
