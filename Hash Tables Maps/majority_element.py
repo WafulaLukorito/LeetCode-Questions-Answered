9fc1c62701b9490aa4215b7a798351ae053b9909
lukorito <jonesdeelder@gmail.com>
2022-09-12 22:10:38 +0300

    reverse linked list

--- Hash Tables Maps/majority_element.py
+++ Hash Tables Maps/majority_element.py
@@ -58,7 +58,10 @@ The majority element is the element that appears more than ⌊n / 2⌋ times. Yo
 #         if m[num] > len(nums)//2:
 #             return num
 
+from collections import namedtuple
 from collections import Counter
+from itertools import accumulate, combinations, combinations_with_replacement, permutations
+import operator
 
 
 def majority_element(nums):
@@ -88,3 +91,59 @@ def majority_eleme(nums):
 nums = [2, 2, 1, 1, 1, 2, 2]
 result = majority_eleme(nums)
 print(f"The majority element in array {nums} is {result} ")
+
+
+def major_ele(arr):
+    myCounter = Counter(arr)
+    return myCounter.most_common(1)[0][0]
+
+
+nums = [2, 2, 1, 1, 1, 2, 2]
+result = major_ele(nums)
+print(f"The majority element in array {nums} is {result} ")
+
+
+my_tuple = namedtuple('my_tuple', 'name age')
+my_tuple.name = 'John'
+my_tuple.age = 30
+print(my_tuple.age)  # prints 30
+
+my_tuple2 = ('John', 30, 'Nairobi', 'Kenya', 'inazidi', 'kuenda',
+             'mbele', 'pamoja', 'tushirikiane', 'tupendane', 'bendera')
+#! print(my_tuple2.find('Kenya'))  #Does not work!! Tuple has no attribute find!
+print(my_tuple2.index('Kenya'))  # prints 3
+
+myList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+myList.reverse()
+print(myList)  # prints [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
+rotated_list = myList[3:] + myList[:3]
+print(rotated_list)  # prints [4, 3, 2, 1, 10, 9, 8, 7, 6, 5]
+
+a1 = [1, 2, 3]
+perm = permutations(a1)
+print(list(perm))
+perm2 = permutations(a1, 2)
+print(list(perm2))
+
+a2 = [1, 2, 3, 4]
+comb = combinations(a2, 3)
+print(list(comb))
+
+comb2 = combinations_with_replacement(a2, 2)
+print(list(comb2))
+
+a3 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+a3acc = accumulate(a3)
+print(list(a3acc))
+
+a3_multipy_cummulatively = list(accumulate(a3, operator.mul))
+print(a3_multipy_cummulatively)
+
+a3_divide_cummulatively = list(accumulate(a3, operator.truediv))
+print(a3_divide_cummulatively)
+print()
+
+a31 = [3, 5, 7, 9, 3, 2, 45, 31, 1]
+# Returns the maximum of the current and previous elements
+a31acc = accumulate(a31, max)
+print(list(a31acc))
