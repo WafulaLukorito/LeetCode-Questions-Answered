8b2df617ab21f73296908837e9a23b04d8f50470
lukorito <jonesdeelder@gmail.com>
2023-01-05 01:15:12 +0300

    singly linked list

--- Hash Tables Maps/majority_element.js
+++ Hash Tables Maps/majority_element.js
@@ -37,14 +37,15 @@ console.log(`The majority element in array ${nums} is ${res}`);
  * @return {number}
  */
 var majorityElement = function(nums) {
-        let m = new Map();
-        for (let i = 0; i < nums.length; i++) {
-            if (m.has(nums[i])) {
-                m.set(nums[i], m.get(nums[i]) + 1)
-            } else {
-                m.set(nums[i], 1)
-            }
-            if (m.get(nums[i]) > nums.length / 2) {
-                return nums[i]
-            }
+    let m = new Map();
+    for (let i = 0; i < nums.length; i++) {
+        if (m.has(nums[i])) {
+            m.set(nums[i], m.get(nums[i]) + 1)
+        } else {
+            m.set(nums[i], 1)
         }
+        if (m.get(nums[i]) > nums.length / 2) {
+            return nums[i]
+        }
+    }
+}
