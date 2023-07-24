813d773966101ebee31fd0ae1a9b6053c67cf1e5
Lukorito <jonesdeelder@gmail.com>
2023-07-24 16:00:28 +0300

    path sum

---
+++ .history/Trees/max_depth_tree_20230724160008.py
@@ -0,0 +1,26 @@
+﻿    """
+    #Leetcode 104. Maximum Depth of Binary Tree
+    #Easy: Tree, DFS, BFS
+    #Link: https://leetcode.com/problems/maximum-depth-of-binary-tree/
+    
+    Given a binary tree, find its maximum depth.
+    The maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+    
+    Note: A leaf is a node with no children.
+    
+    Example:
+    Given binary tree [3,9,20,null,null,15,7], return its depth = 3.
+    
+    """
+
+def max_tree_depth(root):
+    if root is None:
+        return 0
+    else:
+        left_depth = max_tree_depth(root.left)
+        right_depth = max_tree_depth(root.right)
+        
+        if left_depth > right_depth:
+            return left_depth+1
+        else:
+            return right_depth+1  #*Note: +1 is for the root node
---
+++ .history/Trees/symetric_trees_20230724160007.py
@@ -0,0 +1,23 @@
+﻿"""
+Leet Code: 101. Symmetric Tree
+Easy: Tree, DFS, BFS
+Link: https://leetcode.com/problems/symmetric-tree/
+
+Given a binary tree, check whether it is a mirror of itself (ie, symmetric around its center).
+
+Example 1:
+Input: root = [1,2,2,3,4,4,3]
+Output: True
+
+"""
+
+
+def isMirror(root):
+    return _isMirror(root, root)
+
+def _isMirror(node1, node2):
+    if node1==None and node2==None:
+        return True
+    if node1==None or node2==None:
+        return False
+    return((node1.val==node2.val) and (_isMirror(node1.right, node2.left)) and _isMirror(node1.left, node2.right))
