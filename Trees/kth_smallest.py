c3074342b170bd0726d959fe9b07976ee4d26f9c
Lukorito <jonesdeelder@gmail.com>
2023-07-25 07:02:35 +0300

    lca

---
+++ Trees/kth_smallest.py
@@ -0,0 +1,15 @@
+﻿    """
+    Kth Smallest Element in a BST
+    
+    Link: https://leetcode.com/problems/kth-smallest-element-in-a-bst/
+    Medium - Tree, DFS
+    
+    Given a binary search tree, write a function kthSmallest to find the kth smallest element in it.
+    
+    Note:
+    You may assume k is always valid, 1 ≤ k ≤ BST's total elements.
+    
+    Time Complexity: O(N): N is the number of nodes in the tree
+    
+    """
+    
