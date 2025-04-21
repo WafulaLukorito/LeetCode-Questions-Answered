ee468c53ac156ecf95055087f29caeb70c55375a
Lukorito <jonesdeelder@gmail.com>
2025-04-21 13:03:39 +0300

    missing numbe

---
+++ .history/Math/MissingNumber_20250421125317.java
@@ -0,0 +1,30 @@
+
+// """Given an array nums containing n distinct numbers in the range [0, n], return the only number in the range that is missing from the array.
+
+// Follow up: Could you implement a solution using only O(1) extra space complexity and O(n) runtime complexity?
+
+// #? Use Gauss Formula
+
+// #?Time Complexity O(n) as we loop over elements of array once
+
+// #? Space complexity O(1)
+//     """
+
+import java.util.Arrays;
+
+public class MissingNumber{
+    public static int missingNumber(int[] nums){
+     int n = nums.length;
+
+     int intendedSum = n * (n+1)/2;
+
+     int trueSum = Arrays.stream(nums).sum();
+
+     return intendedSum - trueSum;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {9, 6, 4, 2, 3, 5, 7, 0, 1};
+        System.out.println("The missing number: "+missingNumber(nums));
+    }
+}
